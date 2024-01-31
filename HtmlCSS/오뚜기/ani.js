e6ca6816983bb03f36ca4630c4ebf059bf65bcf1
ha <withme1004v@gmail.com>
2024-01-31 14:43:01 +0900

    first commit

--- HtmlCSS/오뚜기/ani.js
+++
@@ -1,7 +0,0 @@
-var anmation = bodymovin.loadAnimation({
-  container: document.getElementById("animContainer"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "ottogi.json", // 2-2. 다운받아서 사용.
-});
