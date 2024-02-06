0be8f11fad96d79d3cbc4dbd37653997d8065b25
ha <withme1004v@gmail.com>
2024-02-06 16:31:18 +0900

    first commit

---
+++ javascript/gggs/js/gggs.js
@@ -0,0 +1,24 @@
+let gnb = document.querySelectorAll(".menu li");
+let lnb = document.querySelectorAll(".lnb");
+let sub = document.querySelectorAll(".sub");
+
+function myfnc() {
+  lnb.forEach(function (keys, item) {
+    keys.style.height = 0 + "px";
+  });
+  document.querySelector("header").style.background = "none";
+  document.querySelector(".logo").classList.remove("on");
+}
+gnb.forEach(function (keys, item) {
+  keys.onmouseenter = function () {
+    sub.forEach(function (o, i) {
+      let ht = sub[item].offsetHeight;
+      lnb[item].style.height = ht + "px";
+    });
+    document.querySelector("header").style.background = "#fff";
+    document.querySelector(".logo").classList.add("on");
+  };
+  keys.onmouseleave = function () {
+    myfnc();
+  };
+});
